1ec94351dcc09f5b153d753936af8d3e7ebf5d20
maxlabz <max.w.sauer@gmx.de>
2025-06-02 17:14:18 +0200

    Update firebase.js

--- Spiele/Nicosus/firebase.js
+++ Spiele/Nicosus/firebase.js
@@ -1,1 +1,16 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
+const firebaseConfig = {
+  apiKey: "DEIN_API_KEY",
+  authDomain: "DEIN_AUTH_DOMAIN",
+  projectId: "DEIN_PROJECT_ID",
+  storageBucket: "DEIN_STORAGE_BUCKET",
+  messagingSenderId: "DEIN_MESSAGING_ID",
+  appId: "DEINE_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+window.auth = auth;
