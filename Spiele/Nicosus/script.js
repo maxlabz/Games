2d087d09952c6ca817962192ce9e065f1ec02d05
maxlabz <max.w.sauer@gmx.de>
2025-06-02 17:15:54 +0200

    Update script.js

--- Spiele/Nicosus/script.js
+++ Spiele/Nicosus/script.js
@@ -1,1 +1,23 @@
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
+const auth = window.auth;
+
+onAuthStateChanged(auth, (user) => {
+  const userLinks = document.getElementById("user-links");
+  const logoutButton = document.getElementById("logout-button");
+
+  if (user) {
+    userLinks.style.display = "none";
+    logoutButton.style.display = "inline-block";
+  } else {
+    userLinks.style.display = "inline-block";
+    logoutButton.style.display = "none";
+  }
+});
+
+document.getElementById("logout-button")?.addEventListener("click", () => {
+  signOut(auth).then(() => {
+    alert("Erfolgreich abgemeldet.");
+    location.reload();
+  });
+});
